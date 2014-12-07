00a72cc536aebb5039da42cbd8ffa84501911292
roquefsneto18@gmail.com <roquefsneto18@gmail.com@f9ab774f-0e0d-32ba-a837-b71c334457ae>
2014-12-07 16:25:54 +0000

    teste ubunutu

--- trunk/SistemaPousada/src/controller/PousadaController.java
+++ trunk/SistemaPousada/src/controller/PousadaController.java
@@ -6,7 +6,6 @@
 package controller;
 
 import dao.PousadaDao1;
-import java.util.List;
 import model.Pousada;
 
 /**
--- trunk/SistemaPousada/src/controller/QuartoController.java
+++ trunk/SistemaPousada/src/controller/QuartoController.java
@@ -5,10 +5,56 @@
  */
 package controller;
 
+import dao.QuartoDao1;
+import java.util.List;
+import model.Quarto;
+
 /**
  *
  * @author qualidade-2
  */
 public class QuartoController {
   
+  public QuartoController() {
+    
+  }
+  
+  public Quarto cadastrar( int numeroQuarto, 
+                           double valorDiaria, 
+                           String tipoCama, 
+                           String tipoClimatizacao ) {
+    
+    Quarto quarto = new Quarto( numeroQuarto, 
+                                valorDiaria, 
+                                tipoCama, 
+                                tipoClimatizacao );
+    
+    QuartoDao1 quartoDao1 = new QuartoDao1();
+    
+    if(quartoDao1.salvar(quarto)) {
+      return quarto;
+    }else{
+      return null;
+    }
+  }
+  
+  public Quarto alterar( int numeroQuarto, 
+                         double valorDiaria, 
+                         String tipoCama, 
+                         String tipoClimatizacao ) {
+    
+    return null;
+  }
+  
+  public List buscarAll( String nome ) {
+    return null;
+  }
+  
+  public Quarto buscar( int id ) {
+    return null;
+  }
+  
+  public Quarto excluir( int id ) {
+    return null;
+  }
 }
