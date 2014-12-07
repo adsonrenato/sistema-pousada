3d25fce2746d6d5686fe07ce704e2a372d2de33d
roquefsneto18 <roquefsneto18@gmail.com>
2014-12-07 16:10:56 +0000

    teste ubunutu

--- SistemaPousada/src/controller/FuncionarioController.java
+++ SistemaPousada/src/controller/FuncionarioController.java
@@ -5,10 +5,45 @@
  */
 package controller;
 
+import dao.FuncionarioDao1;
+import java.util.List;
+import model.Funcionario;
+
 /**
  *
  * @author qualidade-2
  */
 public class FuncionarioController {
   
+  public FuncionarioController() {
+    
+  }
+  
+  public Funcionario cadastrar( String nome, String endereco, double salario ) {
+    
+    Funcionario funcionario = new Funcionario( nome, endereco, salario );
+    FuncionarioDao1 funcionarioDao1 = new FuncionarioDao1();
+    
+    if(funcionarioDao1.salvar(funcionario)) {
+      return funcionario;
+    }else{
+      return null;
+    }
+  }
+  
+  public Funcionario alterar( int id, String nome, String Endereco, double salario ) {
+    return null;
+  }
+  
+  public List buscarAll( String nome ) {
+    return null;
+  }
+  
+  public Funcionario buscar( int id ) {
+    return null;
+  }
+  
+  public Funcionario excluir( int id ) {
+    return null;
+  }
 }
--- SistemaPousada/src/controller/PousadaController.java
+++ SistemaPousada/src/controller/PousadaController.java
@@ -5,10 +5,62 @@
  */
 package controller;
 
+import dao.PousadaDao1;
+import java.util.List;
+import model.Pousada;
+
 /**
  *
  * @author qualidade-2
  */
 public class PousadaController {
+
+  public PousadaController() {
+  
+  }
+  
+  public Pousada cadastrar( String razaoSocial, 
+                            String fantasia, 
+                            String cnpj, 
+                            String telefone, 
+                            String endereco, 
+                            int numEndereco, 
+                            int quantQuarto ) {
+    
+    Pousada pousada = new Pousada(razaoSocial, 
+                                  fantasia, 
+                                  cnpj, 
+                                  telefone, 
+                                  endereco,
+                                  numEndereco, 
+                                  quantQuarto);
+    
+    PousadaDao1 pousadaDao1 = new PousadaDao1();
+    
+    if(pousadaDao1.salvar(pousada)) {
+      return pousada;
+    }else{
+      return null;
+    }
+  }
+  
+  public Pousada alterar( String razaoSocial, 
+                          String fantasia, 
+                          String cnpj, 
+                          String telefone, 
+                          String endereco, 
+                          int numEndereco, 
+                          int quantQuarto ) {
+    
+    return null;
+  }
+    
+  public Pousada buscar( int id ) {
+    return null;
+  }
+  
+  public Pousada excluir( int id ) {
+    return null;
+  }
   
 }
