87df876bb74b7752674baa88418cbd44131c4ad1
roquefsneto18 <roquefsneto18@gmail.com>
2014-12-07 16:25:54 +0000

    teste ubunutu

--- SistemaPousada/src/controller/UsuarioController.java
+++ SistemaPousada/src/controller/UsuarioController.java
@@ -5,10 +5,48 @@
  */
 package controller;
 
+import dao.QuartoDao1;
+import dao.UsuarioDao1;
+import java.util.List;
+import model.Usuario;
+
 /**
  *
  * @author qualidade-2
  */
 public class UsuarioController {
   
+  public UsuarioController() {
+    
+  }
+  
+  public Usuario cadastrar( int id, String nome, int senha ) {
+    
+    Usuario usuario = new Usuario( id, nome, senha );
+    
+    UsuarioDao1 usuarioDao1 = new UsuarioDao1();
+    
+    if(usuarioDao1.salvar(usuario)) {
+      return usuario;
+    }else{
+      return null;
+    }
+  }
+  
+  public Usuario alterar( int id, String nome, int senha ) {
+    
+    return null;
+  }
+  
+  public List buscarAll( String nome ) {
+    return null;
+  }
+  
+  public Usuario buscar( int id ) {
+    return null;
+  }
+  
+  public Usuario excluir( int id ) {
+    return null;
+  }
 }
